fba2d0ae3a343f5084f5b1b3c592c6aa049abb0e
Isaac Ashwin <ashiswin@gmail.com>
2020-10-18 11:05:14 +0800

    Split dummy links into categories
    Split up the dummy links by category and added the ability to render multiple grids with titles.

--- src/components/LinkGrid.tsx
+++ src/components/LinkGrid.tsx
@@ -1,32 +1,44 @@
+import { Icon } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import { DummyLinks } from "../dummy/DummyHomeScreenLinkProvider";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "../dummy/DummyHomeScreenLinkProvider";
 import LinkGridItem from "./LinkGridItem";
 
-const LinkGrid: React.FC = () => {
+interface Props {
+  header: string
+  links: Link[]
+}
+
+const LinkGrid: React.FC<Props> = ({header, links}) => {
     let linkPairs = [];
 
-    for(var i = 0; i < DummyLinks.length; i += 2)
+    // Split the list of links into pairs
+    for(var i = 0; i < links.length; i += 2)
     {
-      linkPairs.push(DummyLinks.slice(i, i + 2));
+      linkPairs.push(links.slice(i, i + 2));
     }
 
-    let links = linkPairs.map((pair) => {
+    let linkComponents = linkPairs.map((pair) => {
         return(
             <Row style={{margin: 0}}>
                 <LinkGridItem title={pair[0].title} url={pair[0].url} icon={pair[0].iconName}/>
                 {
                     pair.length > 1
-                        ? <LinkGridItem  title={pair[1].title} url={pair[1].url} icon={pair[1].iconName}/>
-                        : null
+                      ? <LinkGridItem  title={pair[1].title} url={pair[1].url} icon={pair[1].iconName}/>
+                      : null
                 }
             </Row>
         );
       })
     
     return(
-        <Container fluid style={{padding: 0}}>
-          {links}
+        <Container fluid style={{padding: 0, marginBottom: 8}}>
+          <Row style={{margin: 0}}>
+            <Col xs={12}>
+            <h2>{header}</h2>
+            </Col>
+          </Row>
+          {linkComponents}
         </Container>
     );
 }
