26ac7ad27c60261ba83bbb95e357c5435d5004f4
Isaac Ashwin <ashiswin@gmail.com>
2020-12-04 20:59:55 +0800

    Wire up Firebase to signup screen

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,19 +1,60 @@
 import React, { useState } from "react";
 import { Button, Col, Container, Row, TextInput } from "react-materialize";
 import smota_logo from "../assets/img/smota-logo.png";
+import ErrorMessage from "../components/ErrorMessage";
+import { auth, generateUserDocument } from "../firebase";
 
 const SignUpScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [errorVisible, setErrorVisible] = useState(false);
 
-  const createUserWithEmailAndPasswordHandler = (event: React.MouseEvent<HTMLButtonElement>, name: string, email: string, password: string, confirmPassword: string) => {
+  const createUserWithEmailAndPasswordHandler = async (event: React.MouseEvent<HTMLButtonElement>, name: string, email: string, password: string, confirmPassword: string) => {
     event.preventDefault();
-    setEmail("");
-    setPassword("");
-    setConfirmPassword("");
-    setName("");
+    if (name === "") {
+      setError("Name is empty");
+      setErrorVisible(true);
+      return;
+    }
+    if (email === "") {
+      setError("Email is empty");
+      setErrorVisible(true);
+      return;
+    }
+    if (password === "") {
+      setError("Password is empty");
+      setErrorVisible(true);
+      return;
+    }
+    if (confirmPassword === "") {
+      setError("Password confirmation is empty");
+      setErrorVisible(true);
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      setErrorVisible(true);
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      generateUserDocument(user, { name });
+
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setName("");
+      setError("");
+      setErrorVisible(false);
+    }
+    catch (error) {
+      setError(error.message);
+      setErrorVisible(true);
+    }
   };
 
   const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
@@ -41,6 +82,7 @@ const SignUpScreen = () => {
           <h5>Sign up with St Mary's</h5>
         </Col>
       </Row>
+      <ErrorMessage message={error} visible={errorVisible} onCloseClick={() => setErrorVisible(false)} />
       <TextInput name="name" label="Name" onChange={onChangeHandler} value={name} noLayout />
       <TextInput name="email" label="Email" type="email" onChange={onChangeHandler} value={email} email noLayout />
       <Row style={{ padding: 0, marginTop: -16 }}>
