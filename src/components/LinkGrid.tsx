9e550da735753829b3b52e29345ec003ec3eb11b
Clement <clement_ravindran@hotmail.com>
2021-06-10 00:17:03 +0800

    Edited Buttons

--- src/components/LinkGrid.tsx
+++ src/components/LinkGrid.tsx
@@ -8,38 +8,45 @@ interface Props {
   links: Link[]
 }
 
-const LinkGrid: React.FC<Props> = ({header, links}) => {
-    let linkPairs = [];
+const LinkGrid: React.FC<Props> = ({ header, links }) => {
+  let linkPairs = [];
 
-    // Split the list of links into pairs
-    for(var i = 0; i < links.length; i += 2)
-    {
-      linkPairs.push(links.slice(i, i + 2));
-    }
+  // Split the list of links into pairs
+  for (var i = 0; i < links.length; i += 2) {
+    linkPairs.push(links.slice(i, i + 2));
+  }
 
-    let linkComponents = linkPairs.map((pair, index) => {
-        return(
-          <Row style={{margin: 0}} key={index}>
-              <LinkGridItem title={pair[0].title} url={pair[0].url} icon={pair[0].iconName} key={pair[0].title} />
-              {
-                pair.length > 1
-                  ? <LinkGridItem  title={pair[1].title} url={pair[1].url} icon={pair[1].iconName}/>
-                  : null
-              }
-          </Row>
-        );
-      })
-    
-    return(
-      <div>
-        <Row style={{margin: 0}}>
-          <Col s={12}>
-            <h4>{header}</h4>
-          </Col>
-        </Row>
-        {linkComponents}
-      </div>
+  let linkComponents = linkPairs.map((pair, index) => {
+    return (
+      <Row style={styles.row} key={index}>
+        <LinkGridItem title={pair[0].title} url={pair[0].url} image={pair[0].image} icon={pair[0].iconName} text={pair[0].text} key={pair[0].title} />
+        {
+          pair.length > 1
+            ? <LinkGridItem title={pair[1].title} image={pair[1].image} url={pair[1].url} text={pair[1].text} icon={pair[1].iconName} />
+            : null
+        }
+      </Row>
     );
+  })
+
+  return (
+    <div>
+      <Row style={{ margin: 0 }}>
+        <Col s={12}>
+          <h4>{header}</h4>
+        </Col>
+      </Row>
+      {linkComponents}
+    </div>
+  );
 }
+
+const styles = {
+  row: {
+    margin: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+};
 
 export default LinkGrid;
--- src/components/LinkGridItem.tsx
+++ src/components/LinkGridItem.tsx
@@ -7,21 +7,27 @@ interface Props {
   title: string
   url: string
   icon: IconDefinition
+  image: string
+  text: string
 }
 
 function handleClick(url: string) {
   window.open(url);
 }
 
-const LinkGridItem: React.FC<Props> = ({ title, url, icon }) => (
-  <Col s={6} style={{ padding: '0' }}>
+const LinkGridItem: React.FC<Props> = ({ title, url, icon, image, text }) => (
+  <Col s={6} style={{ padding: '0', marginLeft: "0%" }}>
     <a
       href="#!"
-      className="waves-effect waves-light btn" 
-      style={styles.link} 
+      className="waves-effect waves-light btn"
+      style={styles.link}
       onClick={() => handleClick(url)}>
-      <FontAwesomeIcon icon={icon} />&nbsp;&nbsp;<span>{title}</span>
-      </a>
+      <span style={styles.title}>{title}</span>
+      <br />
+      <img src={image}></img>
+      <br />
+      <span style={styles.text}>{text}</span>
+    </a>
   </Col>
 )
 
@@ -30,14 +36,21 @@ const styles = {
     height: 64,
     backgroundColor: "var(--primary)",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
     border: "0.005vw solid var(--primary-light)",
     color: "white",
     cursor: "pointer",
-    width: '100%',
+    width: '95%',
     lineHeight: '1',
-  }
+    marginLeft: "2.5%",
+    borderRadius: 13,
+  },
+  title: {
+    marginTop: 15,
+  },
+  text: {
+    fontSize: 12,
+    align: "left",
+  },
 };
 
 export default LinkGridItem;
