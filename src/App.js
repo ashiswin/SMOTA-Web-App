2d0f7ca2113c4aa615a02e917ded2c561a027f44
Isaac Ashwin Ravindran <ashiswin@gmail.com>
2021-06-13 17:26:02 -0700

    Made title bar fixed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppUpdateNotification from './components/notifications/AppUpdate';
 import InstallToHomeScreenNotification from './components/notifications/InstallToHomeScreen';
 import TitleBar from './components/TitleBar';
 import BottomTabNavigation from './components/BottomTabNavigation';
+import smota_logo from "./assets/img/smota-logo-full.png";
 const MassVideoListScreen = lazy(() => import("./screens/MassVideoListScreen"));
 const AboutScreen = lazy(() => import("./screens/AboutScreen"));
 const HomeScreen = lazy(() => import("./screens/HomeScreen"));
@@ -28,29 +29,30 @@ const App = ({ isUpdateAvailable, isInstallAvailable, deferredPrompt }) => {
         <InstallToHomeScreenNotification isInstallAvailable={isInstallAvailable} deferredPrompt={deferredPrompt} />
         <TitleBar />
         <div style={{marginBottom: 56}}>
-        <Switch>
-          <Route path="/about">
-            <AboutScreen />
-          </Route>
-          <Route path="/holy-mass">
-            <MassVideoListScreen isSundayMass={true} />
-          </Route>
-          <Route path="/daily-liturgy">
-            <MassVideoListScreen isSundayMass={false} />
-          </Route>
-          <Route path="/signup">
-            <SignUpScreen />
-          </Route>
-          <Route path="/signin">
-            <SignInScreen />
-          </Route>
-          <Route path="/passwordReset">
-            <PasswordResetScreen />
-          </Route>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+          <img src={smota_logo} width="50%" alt="SMOTA Logo" />
+          <Switch>
+            <Route path="/about">
+              <AboutScreen />
+            </Route>
+            <Route path="/holy-mass">
+              <MassVideoListScreen isSundayMass={true} />
+            </Route>
+            <Route path="/daily-liturgy">
+              <MassVideoListScreen isSundayMass={false} />
+            </Route>
+            <Route path="/signup">
+              <SignUpScreen />
+            </Route>
+            <Route path="/signin">
+              <SignInScreen />
+            </Route>
+            <Route path="/passwordReset">
+              <PasswordResetScreen />
+            </Route>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
         </div>
         <BottomTabNavigation />
       </Suspense>
--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -4,7 +4,7 @@ import smota_logo from "../assets/img/smota-logo-full.png";
 const TitleBar: React.FC = () => {
     return (
         <div
-            style={{backgroundColor: "var(--primary)"}}>
+            style={{backgroundColor: "var(--primary)", position: "fixed", top: 0, left: 0, zIndex: 1000}}>
             <img src={smota_logo} width="50%" alt="SMOTA Logo" />
         </div>
     );
