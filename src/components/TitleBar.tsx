ee3a20a9a9c09d19bd0c45ec2f89aa88b28864e6
Isaac Ravindran <ashiswin@gmail.com>
2021-07-05 14:15:29 -0700

    Added back button ability in titlebar

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,6 +1,7 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import smota_logo from "../assets/img/smota-logo-full.png";
 import AppUpdateNotification from '../components/notifications/AppUpdate';
 import InstallToHomeScreenNotification from '../components/notifications/InstallToHomeScreen';
@@ -12,6 +13,7 @@ interface Props {
     isUpdateAvailable: boolean,
 }
 const TitleBar: React.FC<Props> = ({backEnabled, isInstallAvailable, deferredPrompt, isUpdateAvailable}) => {
+    const history = useHistory();
     return (
         <>
             <div style={{position: "fixed", top: 0, left: 0, zIndex: 1000}}>
@@ -21,13 +23,14 @@ const TitleBar: React.FC<Props> = ({backEnabled, isInstallAvailable, deferredPro
                     style={{backgroundColor: "var(--primary)", display: "flex", flexDirection:"row", alignItems: "center", paddingBottom: 4, paddingTop: 4}}>
                     {
                         backEnabled
-                            ? <div style={{width: 32, height: 32}}>
+                            ? <div style={{width: 32, height: 32, cursor: "pointer"}}>
                                 <FontAwesomeIcon
-                                icon={faArrowLeft}
-                                color="white"
-                                size="lg"
-                                style={{position: "relative", top: 8, left: 14}} />
-                                </div>
+                                    icon={faArrowLeft}
+                                    color="white"
+                                    size="lg"
+                                    style={{position: "relative", top: 8, left: 14}}
+                                    onClick={() => {history.goBack()}} />
+                              </div>
                             : null
                     }
                 <img src={smota_logo} width="50%" alt="SMOTA Logo" />
